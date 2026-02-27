17e072a60c86d2ef2b8d58f308a559279ea66201
vipulnandeshwar <nandeshwarvipul3@gmail.com>
2026-02-27 23:35:03 +0530

    Add files via upload

---
+++ negative possitive.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    int a,b,c;
+    printf("Enter Number a: ");
+    scanf("%d",& a);
+     if (a>0){
+        printf("%d is Possitive",a);
+    }
+    else {
+        printf("%d is Negative",a);
+    }
+}
